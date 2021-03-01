665ee42c1be7b22a1559c617aa63b9dc49537969
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2021-03-01 10:56:01 +0700

    task9

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -44,4 +45,26 @@ Route::get('about/class/{theArt}/{thePrice}', function ($theArt,$thePrice){
 
 Route::get('where', function () {
    return Redirect::to('about/directions');
+});
+
+Route::get('/insert', function () {
+    DB::insert('insert into posts(title,body) values (?,?)', ['PHP with Laravel','Laravel is the best framework !']);
+    return 'DONE';
+});
+Route::get('/read', function() {
+    $result = DB::select('select * from posts where id = ?', [1]);
+    //return $result;
+    foreach ($result as $post) {
+        return $post->title;
+    }
+});
+
+Route::get('update', function(){
+    $updated = DB::update('update posts set title= "New Title hihi" where id> ?',[1]);
+    return $updated;
+});
+
+Route::get('delete', function () {
+    $deleted = DB::delete('delete from posts where id = ?',[3]);
+    return $deleted;
 });
